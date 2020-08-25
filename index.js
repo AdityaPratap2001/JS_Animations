d13eed6966e10469acfef00dbdd218a3a30624cb
Aditya <pratapsinghaditya2001@gmail.com>
2020-08-25 21:42:34 +0530

    Added animation to Cards

--- index.js
+++ index.js
@@ -79,3 +79,121 @@ setInterval(function(){
   }
 }, 6);
 
+// RESPONSIBLE FOR DISPLAYING DATE
+let date = new Date();
+const options = {weekday:'long',year:'numeric',month:'long',day:'numeric'};
+dateSection.innerText = date.toLocaleDateString(undefined,options);
+
+let anim_loaded1 = false;
+let anim_loaded2 = false;
+let anim_loaded3 = false;
+
+// This function runs when user scrolls and 
+// the width of device is more than 610px 
+function scrollDisappear(){
+
+  let card_top1 = card1.getBoundingClientRect().top;
+  let card_top2 = card3.getBoundingClientRect().top;
+  let card_top3 = card5.getBoundingClientRect().top;
+
+  // FOR CARD 1 & 2
+  if(card_top1 < 90){
+    card1.classList.add('card_anim');
+    card2.classList.add('card_anim2');
+    anim_loaded1 = true;
+  }
+  else if(card_top1 >= 90){
+    card1.classList.remove('card_anim');
+    card2.classList.remove('card_anim2');
+    anim_loaded1 = false;
+  }
+
+  // FOR CARD 3 & 4
+  if(card_top2 < 90){
+    card3.classList.add('card_anim');
+    card4.classList.add('card_anim2');
+    anim_loaded2 = true;
+  }
+  else if(card_top2 >= 90){
+    card3.classList.remove('card_anim');
+    card4.classList.remove('card_anim2');
+    anim_loaded2 = false;
+  }
+  
+  // FOR CARD 5 & 6
+  if(card_top3 < 90){
+    card5.classList.add('card_anim');
+    card6.classList.add('card_anim2');
+    anim_loaded3 = true;
+  }
+  else if(card_top3 >= 90){
+    card5.classList.remove('card_anim');
+    card6.classList.remove('card_anim2');
+    anim_loaded3 = false;
+  }
+}
+
+// This will execute when user scrolls and screen size is <= 610px
+// when cards are aligned vertically 
+
+let cardone_anim = false;
+function scrollDisappear2(){
+
+  let cardOneHeight = card1.getBoundingClientRect().top;
+  let cardTwoHeight = card2.getBoundingClientRect().top;
+  let cardThreeHeight = card3.getBoundingClientRect().top;
+  let cardFourHeight = card4.getBoundingClientRect().top;
+  let cardFiveHeight = card5.getBoundingClientRect().top;
+  let cardSixHeight = card6.getBoundingClientRect().top;
+
+  if(cardOneHeight < 90 && !cardone_anim){
+    card1.classList.add('card_anim');
+    cardone_anim = true;
+  }
+  else if(window.scrollY<=12){
+    card1.classList.remove('card_anim');
+    cardone_anim = false;
+  }
+  else if(cardOneHeight >= 90 ){
+    card1.classList.remove('card_anim');
+    cardone_anim = false;
+  }
+  if(cardTwoHeight < 90){
+    card2.classList.add('card_anim');
+  }
+  else if(cardTwoHeight >= 90 ){
+    card2.classList.remove('card_anim');
+  }
+  if(cardThreeHeight < 90){
+    card3.classList.add('card_anim');
+  }
+  else if(cardThreeHeight >= 90 ){
+    card3.classList.remove('card_anim');
+  }
+  if(cardFourHeight < 90){
+    card4.classList.add('card_anim');
+  }
+  else if(cardFourHeight >= 90 ){
+    card4.classList.remove('card_anim');
+  }
+  if(cardFiveHeight < 90){
+    card5.classList.add('card_anim');
+  }
+  else if(cardFiveHeight >= 90 ){
+    card5.classList.remove('card_anim');
+  }
+  if(cardSixHeight < 90){
+    card6.classList.add('card_anim');
+  }
+  else if(cardSixHeight >= 90 ){
+    card6.classList.remove('card_anim');
+  }
+
+}
+
+if(screen.width <= 610){
+  window.addEventListener('scroll',scrollDisappear2);
+}
+else{
+  window.addEventListener('scroll',scrollDisappear);
+}
