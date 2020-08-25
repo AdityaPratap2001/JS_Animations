f476dd9b3fa5842c761976497de28dabc6e3079a
Aditya <pratapsinghaditya2001@gmail.com>
2020-08-25 11:40:55 +0530

    Added banner and cards

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const preLoader = document.querySelector('.preloader');
 const dateSection = document.querySelector('.date');
+let card1 = document.querySelector('.card_one');
+let card2 = document.querySelector('.card_two');
+let card3 = document.querySelector('.card_three');
+let card4 = document.querySelector('.card_four');
+let card5 = document.querySelector('.card_five');
+let card6 = document.querySelector('.card_six');
+
+console.log(screen.width);
 
 // Responsible for preLoader
 setTimeout(function(){
   preLoader.classList.add('vanish');
-},2300)
+},3000)
 
-// RESPONSIBLE FOR DISPLAYING DATE
-let date = new Date();
-const options = {weekday:'long',year:'numeric',month:'long',day:'numeric'};
-dateSection.innerText = date.toLocaleDateString(undefined,options);
